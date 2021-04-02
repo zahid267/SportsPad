a43491d1ec12efb722d8e919a81b7301ab1397a8
Gift <56089461+e-giftz@users.noreply.github.com>
2021-04-01 20:30:59 -0400

    Search players and display team

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,54 @@
+const myListContainer = document.querySelector('.list-group');
 var teamsContainerEl = document.querySelector('#nbaTeams-container');
 var teamSearchTerm = document.querySelector('#nbaTeams-search-term');
+var userInput = document.querySelector('#userInput');
+let teamDB = [];
 
 function getTeams() {
     var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387';
-  
+
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        //console.log(data.teams[0].strTeam);
-        for (var i = 0; i < data.teams.length; i++) {
-
-            console.log(data.teams[i].strTeam);
-            var teamName = (data.teams[i].strTeam); 
-            
-            //teams[0].strTeam teams[0].strTeam
-
-            var nbaTeamEl = document.createElement('div');
-            nbaTeamEl.classList = 'list-item flex-row justify-space-between align-center';
-
-            var titleEl = document.createElement('span');
-            titleEl.textContent = teamName;
-
-            nbaTeamEl.appendChild(titleEl);
-      
-    
-        }
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            teamDB = data;
+            console.log("teamDB: ", teamDB);
+            showTeams();
+        });
 }
+
+function showTeams() {
+    //console.log(data.teams[0].strTeam);
+    for (var i = 0; i < teamDB.teams.length; i++) {
+
+        // console.log(teamDB.teams[i].strTeam);
+        var teamName = (teamDB.teams[i].strTeam);
+
+        //teams[0].strTeam teams[0].strTeam
+
+        var nbaTeamEl = document.createElement('div');
+        // nbaTeamEl.setAttribute('class', '')
+        // nbaTeamEl.classList = 'list-item flex-row justify-space-between align-center';
+
+        var titleEl = document.createElement('span');
+        titleEl.textContent = teamName;
+
+        nbaTeamEl.append(titleEl);
+        myListContainer.append(nbaTeamEl);
+
+    }
+}
+
+function searchTeams() {
+    // event.preventDefault();
+  const teamNameSearch = userInput.value
+  console.log("User INput: ", teamNameSearch)
+
+  // loop over DB
+  // team name is db.strTeam (?)
+}
+
 
 getTeams();
 
-
-// fetch('https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387')
-//   .then(response => response.json())
-//   //.then(data => console.log(data));
-//   .then(data => console.log(data[0].strTeam));
