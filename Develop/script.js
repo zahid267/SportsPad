64ddc60656575941bc40bcf4ffc7eda32c6a914f
Muhammad Zahid <mrzahid@rogers.com>
2021-04-08 18:31:13 -0400

    Updating all chnages

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,12 @@
 const myListContainer = document.querySelector('.list-group');
 var teamsContainerEl = document.querySelector('#nbaTeams-container');
 var teamSearchTerm = document.querySelector('#nbaTeams-search-term');
-var userInput = document.querySelector('#nbaTeam');
+var userInput = document.querySelector('#team_name');
+var tNameEl = $('#team_name');
 var teamListEl = $('#team_list');
-var teamNameEl = "";
+var playerNameEl = $('#player_name');
+var playerBtn = $("#playerBtn");
+var teamNameEl = "";  var optionEl = "";
 let teamDB = [];
 var teamList = [];
 var teamNames = [];
@@ -11,17 +14,41 @@ var teamNames = [];
 function getTeams() {
     var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387';
 
-    fetch(requestUrl)
+    fetch(requestUrl, {
+        // The browser fetches the resource from the remote server without first looking in the cache.
+        // The browser will then update the cache with the downloaded resource.
+        cache: 'reload',
+      })
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             teamDB = data;
-            console.log("teamDB: ", teamDB);
+          //  console.log("teamDB: ", teamDB);
             showTeams();
+           // getPlayers()
         });
 }
+function getPlayers(){ /// Not used 
+  
+  var requestUrl = 'https://www.balldontlie.io/api/v1/players?search=all';
+  //var requestUrl = './players.json';
 
+    fetch(requestUrl, {
+        // The browser fetches the resource from the remote server without first looking in the cache.
+        // The browser will then update the cache with the downloaded resource.
+        cache: 'reload',
+      })
+        .then(function (response) {
+          console.log(response)
+            return response.json();
+        })
+        .then(function (data) {
+           // teamDB = data;
+            console.log("playersDB: ", data);
+          //  showTeams();
+        });
+}
 function showTeams() {
     //console.log(data.teams[0].strTeam);
     for (var i = 0; i < teamDB.teams.length; i++) {
@@ -29,26 +56,23 @@ function showTeams() {
         // console.log(teamDB.teams[i].strTeam);
         var teamName = (teamDB.teams[i].strTeam);
         var teamid = teamDB.teams[i].idTeam;
-        
-
-       // var nbaTeamEl = document.createElement('div');
-        // nbaTeamEl.setAttribute('class', '')
-        // nbaTeamEl.classList = 'list-item flex-row justify-space-between align-center';
-
-        //var titleEl = document.createElement('span');
-      //  titleEl.textContent = teamName;
-      teamList[i] = teamName;
-        teamNameEl = $('<li>');
+        teamList[i] = teamName;
+        /*teamNameEl = $('<li>');
         teamNameEl.attr("data-index", i);
         teamNameEl.attr("data-id", teamid);
-       // console.log(teamNameEl)
         teamNameEl.text(teamName);
-      //  nbaTeamEl.append(titleEl);
-        teamListEl.append(teamNameEl);
+        teamListEl.append(teamNameEl);*/
+        
+        tNameEl.append('<option value="' + teamName + '">' + teamName + '</option>');
+       // playerTeamEl.append('<option value="' + teamName + '">' + teamName + '</option>');
 
     }
+   // alert(teamIdName);
+   // alert(tNameEl.innerHTML);
+
     teamNames = teamList;
-    setAutoComplete(teamList)
+    //setAutoComplete(teamList)
+    setPlayerAutoComplete(players);
 }
 
 function searchTeams() {
@@ -71,7 +95,23 @@ function searchTeams() {
     alert("Team name does not exists in NBA league - "+teamNameSearch);
   }
 }
-
+playerBtn.on('click', function(event){
+  event.preventDefault();
+  if(playerNameEl.val() == ""){
+    alert("Please enter the NBA player name first.")
+    return false;
+  }else{
+  //  var team = playerTeamEl.val();
+    var name = playerNameEl.val();
+//    team = team.replaceAll(' ','_');
+    name = name.replaceAll(' ','_');
+    var team = playerNameTeam[name];
+    team = team.replaceAll(' ','_');
+    var queryString = './player.html?q=' + team+'&p='+name;         //searchInputVal + '&format=' + formatInputVal;
+  
+    location.assign(queryString);
+  }
+})
 teamListEl.on('click', function(event){
   event.preventDefault();
   var teamIndex = event.target.dataset.index;
@@ -92,13 +132,18 @@ teamListEl.on('click', function(event){
 
 // Autocomplete widget
 function setAutoComplete(teamListNames){
-  //$(function () {
-   // teamNames = teamList;
-    
     $('#nbaTeam').autocomplete({
       source: teamListNames,
     });
-  //});
 }
+function setPlayerAutoComplete(playerList){
+  $('#player_name').autocomplete({
+    source: playerList,
+  });
+}
+/*fetch("./players.json")
+  .then(response => response.json())
+  .then(json => console.log(json));*/
 
 getTeams();
+
