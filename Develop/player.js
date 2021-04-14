08a521f36e9ab801c48098a314c426bd205800ff
Muhammad Zahid <mrzahid@rogers.com>
2021-04-14 19:30:25 -0400

    Updating all files

--- Develop/player.js
+++ Develop/player.js
@@ -3,9 +3,8 @@ let playerSt = [];
 var teamId = "";
 var teamInput = $('#team_Input')
 var playerInput = $('#player_input')
-
   
-var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=' + teamInput + '&p= ' + playerInput ;
+//var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=' + teamInput + '&p= ' + playerInput ;
 
 
 
@@ -17,32 +16,30 @@ function getParams() {
     getPlayers(team, player);
   }
 
+    //var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Toronto_Raptors&p=Kyle_Lowry';
 
-
-
-    var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Toronto_Raptors&p=Kyle_Lowry';
-
-    
-
-
-    function getPlayers(team, player) {   /// Not used on this page
-        var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t='+team+'&p='+player;
-        console.log(requestUrl);
+function getPlayers(team, player) {   /// Not used on this page
+    var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t='+team+'&p='+player;
+  //player = player.replaceAll('_', ' ');
+    //var requestUrl = 'https://www.balldontlie.io/api/v1/players?search='+player;
+    //var requestUrl = 'https://www.balldontlie.io/api/v1/season_average';
+   // console.log(requestUrl);
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            playerSt = data.player[0];
-            console.log("playerSt: ", playerSt );
-
+            //console.log(data);
+            
             if(data.player == null){
-                alert("No data available for the given criteria.");
-                window.history.back();
+               // alert("No data available for the given criteria.");
+             //   window.history.back();
+                getPlayerBalldontlie(player);   /// Check the other API for player data
                 return false;
             }
-
+            playerSt = data.player[0];
+            //console.log("playerSt: ", playerSt );
 
             var playerBrith = playerSt.strBirthLocation
             $('#city_birth').append("Born: " + playerBrith)
@@ -58,15 +55,65 @@ function getParams() {
             $('#about_player').append("About " + playerName)
             $('#player_desc').append(playerDes)
             var playerTeam = playerSt.strTeam
-            $('#player_team').append(playerTeam)
+            $('#player_team').append("Team : " + playerTeam)
             var playerPic = playerSt.strThumb
             $('#player_pic').attr('src', playerPic)
-            
-            
-            console.log(playerPic)
         })
-
-
-       
     }
+    function getPlayerBalldontlie(player) {  
+      //  var requestUrl = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t='+team+'&p='+player;
+        player = player.replaceAll('_', ' ');
+        var requestUrl = 'https://www.balldontlie.io/api/v1/players?search='+player;
+        //var requestUrl = 'https://www.balldontlie.io/api/v1/season_average';
+       // console.log(requestUrl);
+    
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+               // console.log(data);
+               // playerSt = data[0];
+               // console.log("playerBalldontlie: ", data );
+    
+                if(data.data.length <= 0){
+                    alert("No data available for the given criteria.");
+                    window.history.back();
+                    return false;
+                }else{
+                    data = data.data[0];
+                   // console.log(data);
+                   // console.log("id-data : " + data.first_name);
+                    // pdata = data;
+                    /*  var playerBrith = playerSt.strBirthLocation
+                    $('#city_birth').append("Born: " + playerBrith)*/
+                    var playerName = data.first_name+" "+data.last_name;
+                    
+                    if(data.weight_pounds != null){
+                        var playerWT = data.weight_pounds
+                        $('#weight').append('Weight: ' + playerWT)
+                    }
+                    $('#player-name').append(playerName)
+                    if(data.height_feet != null){
+                        var playerHT = data.height_feet+"' "+data.height_inches+'"';
+                        $('#height').append("Height: " + playerHT)
+                    }
+                    if(data.position != null){
+                        var playerPos = data.position
+                        $('#gender').append("Position: " + playerPos)
+                    }
+                    var playerDes = ""; //playerSt.strDescriptionEN
+                    $('#about_player').append("About " + playerName)
+                    $('#player_desc').append(playerDes)
+                    var playerTeam = data.team.full_name;
+                    $('#player_team').append("Team : " + playerTeam)
+                    var playerPic = 'https://nba-players.herokuapp.com/players/'+data.first_name+'/'+data.last_name;
+                    $('#player_pic').attr('src', playerPic)
+                }
+            })
+        }
+    
+
+
+
     getParams();
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Sports-Reel
+# SportsPad
+06-Server-Side_APIs - 07-Project #1
 A platform where sport fans can access information about the game and the personalities in the specified sport.
+
+This application uses the [theSportsdb API](https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php) to retrieve NBA team's data for NBA teams. The `sessionStorage` is used to store any persistent data.
+
+## User Story
+
+```
+AS A user
+A user WANT to see the NBA stats for multiple teams
+OR a user wants to check info about a specific NBA player
+SO THAT he/she can see latest news about teams and/or players.
+```
+
+## Criteria
+
+```
+GIVEN a Sports Pad is presented with form inputs
+WHEN a user select a team for the team dropdown field
+THEN he/she is presented with the latest 5 events/matches for selected team and an about the team text, the team's stadium information is displayed.The social media links are also shown for the given team.
+WHEN the user view the team scores for the latest events
+THE winning scores are presented with a checkmark, for the given team.
+A RETURN Home button takes the user back to the application's home page.
+GIVEN a user wants to search for a specific player form NBA teams.
+A user starts typing the name of the player.
+A list of player names matching with entered letters is presented
+USER can select a desired name from the autofill list names.
+THEN the user clicks on the "SEARCH NBA PLAYER" button
+THEN the user is presented with the player name, the birthplace, his height, weight, a picture and an about paragraph about the player.
+
+WHEN the user click on the Return Home button on the players page.
+THEN the user is then taken back to the home page
+The user can then repeat his/her search for another team or player.
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The Sports pad app includes a search option for NBA teams, a page about a selected NBA team, and a page about an NBA player.](./images/Sports_pad_demo.gif)
+
+## Review
+
+* The URL of the deployed application : https://zahid267.github.io/WeatherDashboard/
+
+* The URL of the GitHub repository : https://github.com/zahid267/WeatherDashboard
+
+- - -
+*** Developed by Muhammad Zahid --- April 02, 2021
